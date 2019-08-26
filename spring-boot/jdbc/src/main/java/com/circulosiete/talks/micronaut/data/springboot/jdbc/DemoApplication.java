f879d2de73e1f5096ac1a49b036be4976a6b8869
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-08-25 21:09:00 -0500

    wip

--- spring-boot/jdbc/src/main/java/com/circulosiete/talks/micronaut/data/springboot/jdbc/DemoApplication.java
+++ spring-boot/jdbc/src/main/java/com/circulosiete/talks/micronaut/data/springboot/jdbc/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.circulosiete.talks.micronaut.data.springboot.jdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
