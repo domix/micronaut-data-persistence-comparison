2f1f20d5fffcf530e2ac9042344226c894b1ccb5
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-08-26 13:48:18 -0500

    added Micronaut Data JDBC demo

---
+++ micronaut/mn-data-jdbc/src/main/java/com/circulosiete/talks/micronaut/data/jdbc/Application.java
@@ -0,0 +1,10 @@
+package com.circulosiete.talks.micronaut.data.jdbc;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
---
+++ micronaut/mn-data-jpa/src/main/java/com/circulosiete/talks/micronaut/data/jdbc/PeopleService.java
@@ -0,0 +1,16 @@
+package com.circulosiete.talks.micronaut.data.jdbc;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class PeopleService {
+  private final PeopleRepository peopleRepository;
+
+  public PeopleService(PeopleRepository peopleRepository) {
+    this.peopleRepository = peopleRepository;
+  }
+
+  public Person create(Person person) {
+    return peopleRepository.save(person);
+  }
+}
