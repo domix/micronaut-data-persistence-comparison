5c1440983e573a3b19cb17c7aab2c0007bdef63a
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-08-26 17:53:21 -0500

    wip

--- micronaut/mn-data-jpa/src/main/java/com/circulosiete/talks/micronaut/data/jdbc/PeopleRepository.java
+++ micronaut/mn-data-jpa/src/main/java/com/circulosiete/talks/micronaut/data/jdbc/PeopleRepository.java
@@ -1,10 +1,8 @@
 package com.circulosiete.talks.micronaut.data.jdbc;
 
-import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.PageableRepository;
 
-import static io.micronaut.data.model.query.builder.sql.Dialect.POSTGRES;
-
-@JdbcRepository(dialect = POSTGRES)
+@Repository
 public interface PeopleRepository extends PageableRepository<Person, Long> {
 }
